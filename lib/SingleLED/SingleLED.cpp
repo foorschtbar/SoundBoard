f9a9ae6c1dbef7459531b8d290d4a77924cb76cd
foorschtbar <mail@wischmopp90.com>
2024-10-14 22:42:39 +0200

    Improved SingeLED lib

--- lib/SingleLED/SingleLED.cpp
+++ lib/SingleLED/SingleLED.cpp
@@ -1,24 +1,27 @@
 #include "SingleLED.h"
 
-SingleLED::SingleLED(Adafruit_NeoPixel &pixels)
+SingleLED *SingleLED::_instance = nullptr;
+
+SingleLED::SingleLED(int16_t pin, neoPixelType t) : _pixels(new Adafruit_NeoPixel(1, pin, t))
 {
-    _pixels = pixels;
+    _instance = this;
 }
 
 void SingleLED::begin()
 {
-    _pixels.begin();
+    _pixels->begin();
 }
 
 void SingleLED::setColor(int r, int g, int b)
 {
-    _pixels.setPixelColor(0, r, g, b);
-    _pixels.show();
+    _pixels->setPixelColor(0, r, g, b);
+    _pixels->show();
 }
 
 void SingleLED::saveColor()
 {
-    _color = _pixels.getPixelColor(0);
+    _color = _pixels->getPixelColor(0);
+    
 }
 
 void SingleLED::toggleColor(int r, int g, int b)
@@ -26,30 +29,44 @@ void SingleLED::toggleColor(int r, int g, int b)
     static boolean toggle;
     if(toggle)
     {
-        _pixels.setPixelColor(0, 0, 0, 0);
+        _pixels->setPixelColor(0, 0, 0, 0);
         toggle = false;
     }
     else
     {
-        _pixels.setPixelColor(0, r, g, b);
+        _pixels->setPixelColor(0, r, g, b);
         toggle = true;
     }
-    _pixels.show();
+    _pixels->show();
 }
 
 void SingleLED::restoreColor()
 {
-    _pixels.setPixelColor(0, _color);
-    _pixels.show();
+    _pixels->setPixelColor(0, _color);
+    _pixels->show();
 }
 void SingleLED::off()
 {
-    _pixels.setPixelColor(0, 0, 0, 0);
-    _pixels.show();
+    _pixels->setPixelColor(0, 0, 0, 0);
+    _pixels->show();
 }
 
 void SingleLED::setBrightness(int brightness)
 {
-    _pixels.setBrightness(brightness);
+    _pixels->setBrightness(brightness);
 }
 
+void SingleLED::wink(int ms)
+{
+    if(!_locked)
+    {
+        _locked = true;
+        saveColor();
+        off();
+        _ticker.once_ms(ms, []()
+                        {
+            SingleLED::instance()->_locked = false;
+            SingleLED::instance()->restoreColor();
+     });
+    }
+}
--- lib/SingleLED/SingleLED.h
+++ lib/SingleLED/SingleLED.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Ticker.h>
 
 class SingleLED
 {
 public:
-    SingleLED(Adafruit_NeoPixel &);
+    inline static SingleLED *instance()
+    {
+        return _instance;
+    }
+    SingleLED(int16_t pin, neoPixelType t);
     void begin();
     void setColor(int r, int g, int b);
     void off();
@@ -13,8 +18,12 @@ public:
     void toggleColor(int r, int g, int b);
     void saveColor();
     void restoreColor();
+    void wink(int ms);
 
 private:
-    Adafruit_NeoPixel _pixels;
+    static SingleLED *_instance;
+    Adafruit_NeoPixel* _pixels;
+    Ticker _ticker;
     uint32_t _color;
+    bool _locked;
 };
